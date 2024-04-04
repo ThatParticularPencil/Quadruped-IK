05c494773ed87ca9f8dc7d26476184d7a3bce2b3
ThatParticularPencil <megamer30.000@gmail.com>
2024-04-04 18:59:07 -0500

    IK Fixes
    Minor fixes
    - imported tangent
    - changed some joint info
    - better Pavg function
    
    Major changes:
    - Constrain function
    - fixed FK with a diff solution
    - calculated reachability
    - axed Hip and Knee calculations
    - axed Knee polarity with FK check
    - One leg test stuff added

--- IK testing.py
+++ IK testing.py
@@ -1,13 +1,14 @@
-from math import sin,cos,asin,acos,atan,sqrt,degrees,radians,pi
+from math import sin,cos,tan,asin,acos,atan,sqrt,degrees,radians,pi
+import time
 
 # variables
-RestPos = (0,0,0)     #(x,y,z)
+RestPos = (10,-10,0)     #(x,y,z)
 Lerp_Step = 2
 #OffSets = (Hip, Trechanter, Knee)
 Joint_Data = { 
-    0 : (-45,90,"Pelvis"),
-    1 : (0,135,"Hip"),
-    2 : (-150,5,"Knee")
+    0 : (-45,60,"Pelvis"),
+    1 : (-120,120,"Hip",6),
+    2 : (-165,165,"Knee",3)
 }
 
 
@@ -30,23 +31,20 @@ def DTR(angle):
         deg -= 2*pi
     return deg
 
+def Constrain(angle):
+    if angle > 180 or angle < -180:
+        while angle > 180: 
+            angle -= 360
+        while angle < -180:
+            angle += 360
+    return angle
 def Check_MotorAngles(angles):
     ConstrainedAngles = []
     Checked_Angles = []
-
     #forces all the angles between -180 and 180
     for joint in angles:
-        dummy = joint
-        if dummy > 180 or dummy < -180:
-            while dummy > 180 or dummy < -180:
-                if dummy > 180:
-                    dummy -= 360
-                    ConstrainedAngles.append(dummy)
-                elif dummy < -180:
-                    dummy += 360
-                    ConstrainedAngles.append(dummy)
-        else:
-            ConstrainedAngles.append(joint)
+        Constrain(joint)
+        ConstrainedAngles.append(joint)
 
     #P, H, K = ConstrainedAngles[0], ConstrainedAngles[1], ConstrainedAngles[2]
 
@@ -65,97 +63,84 @@ def Check_MotorAngles(angles):
 
     return Checked_Angles
 
+def FK(angles):
+    Pelvis,Hip, Knee = angles[0],angles[1],angles[2]
+
+    #step
+    pos = [Coxa,0,0]
+    pos[0] += Femur*cos(DTR(Hip))
+    pos[1] += Femur*sin(DTR(Hip))
+
+    #step again
+    pos[0] += Tibia*cos(DTR(Hip+Knee))
+    pos[1] += Tibia*sin(DTR(Hip+Knee))
+
+    pos = [round(pos[0],3),round(pos[1],3),0]
+
+    #solve z axis
+    pos[2] += round((pos[0] * tan(Pelvis)),3)
+
+    return pos
+
 def IK(POS):
     x,y,z = POS[0],POS[1],POS[2]
     # D is the hypotenuse of xz plane
     # H is the hypotenuse of the Dy plane
     D = round(sqrt(x**2 + z**2),3)
-    pos = (D,y,z)
     if D==0:
         Pelvis = 0
     else:
-        Pelvis =  round(RTD(asin(z / D)),3)
+        Pelvis =  Constrain(round(RTD(asin(z / D)),3))
 
-    H = sqrt(((D - Coxa)**2) + y**2)
+
+    #2dof y-d plane:
+    
+    #changes D to be relative to coxa
+    D -= Coxa
+
+    # Check if the target is reachable.
+    C = (D**2 + y**2 - Femur**2 - Tibia**2) / (2*Femur*Tibia)
+    # If C > 1, it means the target is outside the workspace of the robot
+    # If C < -1, it means the target is inside the workspace, but not reachable
+    if C > 1 or C < -1:
+        print("!!TARGET IS NOT REACHABLE!!\n")
+
+    H = sqrt((D**2) + y**2)
 
     #HipComp is the angle between H and the x-axis
     #HipInr is the interior angle of Femur-Hip-H
     HipComp = RTD(asin(y/H))
-    #print(H,"\n",(Tibia**2 - (Femur**2 + H**2 )) / (-2*Femur*H))
-    HipInr = RTD(acos((Tibia**2 - (Femur**2 + H**2 )) / (-2*Femur*H)))
-    Hip = round((HipComp+HipInr),3)
+    HipInr = RTD(acos((Tibia**2 - Femur**2 - H**2 ) / (-2*Femur*H)))
+    Hip = Constrain(round((HipComp+HipInr),3))
 
-    #Solve for the Position of the knee with the hip to fix the polarity of its angle
-    #creates a line for the femur to calulate positive of negative knee angles:
-    def FLP(pos,KneePosition,SolveFor ): #Femur Line Prediction
-        d,y,z = pos[0],pos[1],pos[2]
-        PosD,PosY = KneePosition[0],KneePosition[1]
+    #fix for positions behind Hip joint
+    if D <= 0:
+        print("reached D<0")
+        Hip = round((Constrain(180 - Hip)),3)
 
-        slope = (PosY-0)/(PosD-Coxa)
-        intercept = -1*slope*Coxa
-
-        if SolveFor == "x":
-            if slope != 0:
-                return (y-intercept)/slope
-            else:
-                return 25.397
-        else:
-            return (slope*d)+intercept
-        
-    KneePos = round(Femur*RTD(cos(Hip))+Coxa,3),round(Femur*RTD(sin(Hip)),3)
-    #print(KneePos)
+    #Solves Knee
     KneeInterior = round(RTD(acos((H**2 - (Tibia**2 + Femur**2 )) / (-2*Tibia*Femur))),3)
-    #print(round(FLP(pos,KneePos,"x"),3), round(FLP(pos,KneePos,"y"),3))
-    if KneePos[0] < round(FLP(pos,KneePos,"x"),3):
-        Knee = round(180 - (KneeInterior * -1),3)
-        #print("reached, less than x")
-    elif KneePos[1] > round(FLP(pos,KneePos,"y"),3):
-        Knee = round(180 - (KneeInterior * -1),3)
-        #print("reached, greater than y") 
-    else:   
-        Knee = 180 - KneeInterior
+    polarity = 1
+    Knee = Constrain(polarity * round((180 - KneeInterior),3))
+    if abs(FK((Pelvis,Hip,Knee))[0]-x) > 2 or abs(FK((Pelvis,Hip,Knee))[1]-y) > 2:
+        polarity = -1
+        Knee = Constrain(polarity * round((180 - KneeInterior),3))
 
-    #print (Pelvis,Hip, Knee, "\n")
+
     return (Pelvis, Hip, Knee)
-def FK(angles):
-    Pelvis, Hip, Knee = angles[0],angles[1],angles[2]
-    KneeInr = 180-Knee
-    '''organization:
-    - find the length of D
-    - find y
-    - solve z axis
-    '''
-    #length of H with law of cos
-    H = (sqrt((Femur**2 +Tibia**2)-2*Femur*Tibia*(cos(DTR(KneeInr)))))
 
-    #interior angle of the Hip with law of sin
-    HipInr = -(RTD(asin((sin(DTR(KneeInr))/H) * Tibia)))
-    HipComp = Hip - HipInr
 
-    #length of D
-    D = H * (cos(DTR(HipComp))) + Coxa
-
-    #find y
-    y = round((H * sin(DTR(HipComp))),3)
-
-    #solve z axis
-    x = round((D * cos(DTR(Pelvis))),3)
-    z = round((D * sin(DTR(Pelvis))),3)
-
-    return (x,y,z)
-
+def Pavg(points):
+    Xs = [point[0] for point in points]
+    Ys = [point[1] for point in points]
+    Zs = [point[2] for point in points]
+    newpoint = ((round((sum(Xs)/len(Xs)),3)) , (round((sum(Ys)/len(Ys)),3)) , (round((sum(Zs)/len(Zs)),3)))
+    return newpoint
 def interpolate(points):
-    def Pavg(point1,point2):
-        newpoint = []
-        for i in range(3):
-            avg = (point1[i] + point2[i])/2
-            newpoint.append(round(avg,3))
-        return newpoint
-
     def RepLerp(points):
         Path = []   
         for i in range(len(points)-1):
-            Path += [points[i],tuple(Pavg(points[i],points[i+1]))]
+            Path += [points[i],tuple(Pavg((points[i],points[i+1])))]
         Path.append(points[-1])
         return Path
     
@@ -165,10 +150,56 @@ def interpolate(points):
 
     return LerpPath
 
+########################################################################
 
+# main
+def One_Leg_Test():
+    #axis to test
+    testingX = False
+    testingY = False
+    testingZ = True
+
+    while True:
+        pathX = ((10,-5,0),(20,-5,0))    #(start),(end)
+        pathY = ((10,-10,0),(10,-3,0))
+        pathZ = ((10,-5,-5),(10,-5,5))
+        ActivePaths = [None,None,None]  #xyz
+        Keyframes = []                  #(start),(end)
+        starts = []                     #((x,y,z),(x,y,z))
+        ends = []                       #((x,y,z),(x,y,z))
+        frames = []                     #((x,y,z),(...))
+        angles = []                     #((P,H,K),(...))
+
+        if testingX == True:
+            ActivePaths[0] = pathX
+        else:
+            ActivePaths[0] = None
+
+        if testingY== True:
+            ActivePaths[1] = pathY
+        else:
+            ActivePaths[1] = None
+        if testingZ == True:
+            ActivePaths[2] = pathZ
+        else:
+            ActivePaths[2] = None
+
+        for path in ActivePaths:
+            if path != None:
+                starts.append(path[0])
+                ends.append(path[1])
+
+        if len(starts)>0 and len(ends)>0:
+            Keyframes = (Pavg(starts),Pavg(ends))
+
+        frames = interpolate(Keyframes)
+        print(frames)
+        angles = [Check_MotorAngles(IK(frame)) for frame in frames]
+        print(angles)
+        return angles   
+
+
+One_Leg_Test()
+#print(IK((0,0,0)))
 print("\n\n")
-
-
-
-
-#archived
+#print(FK((0,184,142)))
